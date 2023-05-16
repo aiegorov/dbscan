63b6e120ab645f1355a8609a7c6dd2b81a983941
Andrii Iegorov <andrii.iegorov.mm@gmail.com>
2023-05-17 00:24:45 +0200

    pushing back to labels_slices()

--- cpp/dbscan.cpp
+++ cpp/dbscan.cpp
@@ -27,6 +27,7 @@ auto Dbscan::fit_predict(std::vector<Dbscan::Point> const& points) -> std::vecto
     std::vector<std::vector<Dbscan::Point>> points_in_slices(x_slices.size() - 1);
 
     labels_slices.clear();
+    labels_slices.push_back({});
     labels_outputs.clear();
 
     // sorting points into the areas (slices)
