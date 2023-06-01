affd81350727749f1696c17320eaee200d84b237
Andrii Iegorov <andrii.iegorov.mm@gmail.com>
2023-06-01 17:29:29 +0200

    revert

--- cpp/dbscan.cpp
+++ cpp/dbscan.cpp
@@ -64,9 +64,9 @@ auto Dbscan::fit_predict(std::vector<Dbscan::Point> const& points) -> std::vecto
     std::cerr << "sorting took " << std::chrono::duration_cast<std::chrono::microseconds>(ts3 - ts2).count() << "mcs" << std::endl;
 
 // this loop will be parallelized
-size_t i{0};
-#pragma omp parallel num_threads(x_slices_.size() - 1)
-    for (i = 0; i < x_slices_.size() - 1; ++i) {
+//size_t i{0};
+#pragma omp parallel //num_threads(x_slices_.size())
+    for (int i = 0; i < x_slices_.size() - 1; ++i) {
         labels_outputs.at(i) = fit_predict_single(points_in_slices.at(i));
 
 //        labels_outputs.push_back(fit_predict_single(points_in_slice));
