9690e42912f2863ec9557d3773deecbb1f2ed7aa
Andrii Iegorov <andrii.iegorov.mm@gmail.com>
2023-06-26 18:44:12 +0200

    printing aggergate durations

--- cpp/dbscan.cpp
+++ cpp/dbscan.cpp
@@ -38,6 +38,10 @@ void Dbscan::update_durations_(std::uint32_t new_duration){
 
 auto Dbscan::fit_predict(std::vector<Dbscan::Point> const& points) -> std::vector<Dbscan::Label>
 {
+    std::vector<unsigned long long> aggregate_pre_process_ns(points.size());
+    std::vector<unsigned long long> aggregate_loop1_ns(points.size());
+    std::vector<unsigned long long> aggregate_cp_part_ns(points.size());
+
     std::cerr << "Got points" << std::endl;
 
     labels_.assign(std::size(points), undefined);
@@ -121,6 +125,7 @@ auto Dbscan::fit_predict(std::vector<Dbscan::Point> const& points) -> std::vecto
 
     #pragma omp parallel for
     for (auto i = 0UL; i < std::size(new_points); ++i) {
+        const auto ts_before_pre_process = std::chrono::system_clock::now();
         auto const& pt = new_points[i];
         auto const bin_x = static_cast<std::int32_t>(std::floor((pt[0] - min[0]) / eps));
         auto const bin_y = static_cast<std::int32_t>(std::floor((pt[1] - min[1]) / eps));
@@ -129,6 +134,11 @@ auto Dbscan::fit_predict(std::vector<Dbscan::Point> const& points) -> std::vecto
 
         constexpr std::array<int, 9> dx = {-1, +0, +1, -1, +0, +1, -1, +0, +1};
         constexpr std::array<int, 9> dy = {-1, -1, -1, +0, +0, +0, +1, +1, +1};
+        const auto ts_after_pre_process = std::chrono::system_clock::now();
+        const unsigned long pre_process_duration = std::chrono::duration_cast<std::chrono::nanoseconds>(ts_after_pre_process - ts_before_pre_process).count();
+        aggregate_pre_process_ns.at(i) = pre_process_duration;
+
+        const auto ts_before_loop1 = std::chrono::system_clock::now();
         for (auto ni = 0; ni < 9; ++ni) {
             auto const nx = bin_x + dx[ni];
             auto const ny = bin_y + dy[ni];
@@ -147,6 +157,12 @@ auto Dbscan::fit_predict(std::vector<Dbscan::Point> const& points) -> std::vecto
                 }
             }
         }
+        const auto ts_after_loop1 = std::chrono::system_clock::now();
+        const unsigned long loop1_duration = std::chrono::duration_cast<std::chrono::nanoseconds>(ts_after_loop1 - ts_before_loop1).count();
+        aggregate_loop1_ns.at(i) = loop1_duration;
+
+
+        const auto ts_before_cp_part = std::chrono::system_clock::now();
         if (std::size(local_neighbors) > min_samples_) {
             for (auto const n : local_neighbors) {
                 for (auto cp_id{0U}; cp_id < core_points_ids.at(n).size(); ++cp_id) {
@@ -157,7 +173,20 @@ auto Dbscan::fit_predict(std::vector<Dbscan::Point> const& points) -> std::vecto
                 }
             }
         }
+        const auto ts_after_cp_part = std::chrono::system_clock::now();
+        const unsigned long cp_part_duration  = std::chrono::duration_cast<std::chrono::nanoseconds>(ts_after_cp_part - ts_before_cp_part).count();
+        aggregate_cp_part_ns.at(i) = cp_part_duration;
     }
+
+    auto aggr_duration = std::accumulate(aggregate_pre_process_ns.begin(), aggregate_pre_process_ns.end(), 0);
+    std::cerr << "aggregate pre-process duration " << aggr_duration / 1e6 << " ms" << std::endl;
+
+    aggr_duration = std::accumulate(aggregate_loop1_ns.begin(), aggregate_loop1_ns.end(), 0);
+    std::cerr << "aggregate loop1 duration " << aggr_duration / 1e6 << " ms" << std::endl;
+
+    aggr_duration = std::accumulate(aggregate_cp_part_ns.begin(), aggregate_cp_part_ns.end(), 0);
+    std::cerr << "aggregate cp part duration " << aggr_duration / 1e6 << " ms" << std::endl;
+
     const auto now_2 = std::chrono::system_clock::now();
     const std::uint32_t duration = std::chrono::duration_cast<std::chrono::milliseconds>(now_2 - now_1).count();
     std::cerr << "Block in question took " << duration << " ms" << std::endl;
